2a2e6e4653bac0194f3e3b1a8ebe4b497c61d340
anges-bai <45257069+anges-bai@users.noreply.github.com>
2020-08-17 20:28:31 +0800

    Create divide5.py

---
+++ divide5.py
@@ -0,0 +1,10 @@
+value = []
+print('请输入逗号分隔的4位二进制数：')
+items=[x for x in input().split(',')]
+for p in items:
+    intp = int(p, 2)
+    # print(intp)
+    if not intp%5:
+        value.append(p)
+ 
+print (','.join(value))
