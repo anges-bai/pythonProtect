6004c9c692f6dcdd8b897b4ac833d60f579d5db4
anges-bai <45257069+anges-bai@users.noreply.github.com>
2020-08-23 22:10:21 +0800

    Create printList.py

---
+++ printList.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+class ListNode:
+     def __init__(self, x):
+         self.val = x
+         self.next = None
+
+class Solution:
+    def printListFromTailToHead(self, listNode):
+        l =[]
+        while listNode:
+            l.append(listNode.val)
+            listNode = listNode.next
+        return l[::-1]
