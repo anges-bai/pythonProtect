afad30424f79395e0bf0660e0a0691e0e25c7ed1
anges-bai <45257069+anges-bai@users.noreply.github.com>
2020-08-25 21:10:40 +0800

    Update sort2.py

--- sort2.py
+++ sort2.py
@@ -1,1 +1,11 @@
-
+#插入排序
+for i in range(1,len(num)):
+    temp = num[i]
+    for j in range(i,-1,-1):
+        if num[j-1] > temp:
+            num[j] = num[j-1]
+        else:
+            break
+    if i !=j:
+        num[j] = temp
+print(num)
