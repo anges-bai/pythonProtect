6a707de0499689b64bcd2bdc7e24b5d0d7ba1a89
anges-bai <45257069+anges-bai@users.noreply.github.com>
2020-08-25 21:13:23 +0800

    Update sort5.py

--- sort5.py
+++ sort5.py
@@ -1,1 +1,11 @@
-
+#选择排序
+k = 0
+for i in range(len(num)):
+    temp = num[i]
+    for j in range(i+1,len(num)):
+        if num[j] < temp:
+            temp = num[j]
+            k = j
+    if num[i] > num[k]:
+        num[i],num[k] = num[k],num[i]
+print(num)
